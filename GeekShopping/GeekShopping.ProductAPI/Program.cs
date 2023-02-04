aa5f5142235d7e42f5ae37951fa99993c670833e
NotBuer <rodrigosoocer@gmail.com>
2023-02-04 18:10:32 -0300

    @NotBuer -> ProductVO & ProductRepository

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,9 +1,19 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-var connection = configuration["MySQLConnection:MySQLConectionString"];
+var connection = configuration["MySQLConnection:ConnectionString"];
 services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 31))));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+services.AddSingleton(mapper);
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+services.AddScoped<IProductRepository, ProductRepository>();
 
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
